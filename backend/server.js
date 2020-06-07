81041134e4d21272de8c0807fdda36ad7da43c06
Oscar <oscarsrueda96@gmail.com>
2020-06-07 21:48:06 +0200

    Test for backend works!

--- backend/server.js
+++ backend/server.js
@@ -22,3 +22,5 @@ const routes = require("./routes/routes.js")(app, fs);
 const server = app.listen(3000, () => {
   console.log("listening on port %s...", server.address().port);
 });
+
+module.exports = app
--- backend/test/unittest.test.js
+++ backend/test/unittest.test.js
@@ -1,12 +1,11 @@
 var assert = require('assert');
 var request = require('supertest')
+var app = require('../server');
 
-var request = request("http://localhost:3000")
-
-describe('products', function() {
+describe('phones', function() {
     describe('GET', function(){
         it('Should return json as default data format', function(done){
-            request.get('/api/phones')
+            request(app).get('/phones')
                 .expect('Content-Type', /json/)
                 .expect(200, done);
         });
