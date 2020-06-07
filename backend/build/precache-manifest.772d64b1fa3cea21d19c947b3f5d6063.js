74b11a079b8c00c5a2c8d153cc55fdf347d77dee
Oscar <oscarsrueda96@gmail.com>
2020-06-07 19:15:22 +0200

    Dockerize the app

---
+++ backend/build/precache-manifest.772d64b1fa3cea21d19c947b3f5d6063.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8486c79d550aefb90136ceebfb821e57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "332b999b86446ec0b47e",
+    "url": "/static/css/main.f70cf376.chunk.css"
+  },
+  {
+    "revision": "4fb68fb7eeebb28a59b6",
+    "url": "/static/js/2.1d1adede.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1d1adede.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "332b999b86446ec0b47e",
+    "url": "/static/js/main.991638c0.chunk.js"
+  },
+  {
+    "revision": "b85604f279c735a4575a",
+    "url": "/static/js/runtime-main.adcef4e8.js"
+  },
+  {
+    "revision": "7c5cc0694c2689fb4cd88a77555551be",
+    "url": "/static/media/Galaxy_S7.7c5cc069.png"
+  },
+  {
+    "revision": "d2ddcc1f0de7e64d2c937650343e2367",
+    "url": "/static/media/Honor_10.d2ddcc1f.png"
+  },
+  {
+    "revision": "75be282d134b6824f14e4fe34ebd48fa",
+    "url": "/static/media/IPhone_7.75be282d.png"
+  },
+  {
+    "revision": "ce64127c11018b011851d195fcb3f774",
+    "url": "/static/media/Moto_G6.ce64127c.png"
+  },
+  {
+    "revision": "40f20f6f3abb639e9c7a19271d44a864",
+    "url": "/static/media/P10_Lite.40f20f6f.jpg"
+  },
+  {
+    "revision": "b056a898090d21d28cd70618612b35b5",
+    "url": "/static/media/Xiaomi_MI_A2.b056a898.jpeg"
+  },
+  {
+    "revision": "cb5f98a363baeab1ec5c9a015f8cb9ea",
+    "url": "/static/media/ZenPhone_5.cb5f98a3.jpg"
+  },
+  {
+    "revision": "d8d3997b3bc4fba079f4c6b99530ca67",
+    "url": "/static/media/catalogue.d8d3997b.jpeg"
+  },
+  {
+    "revision": "f1a537c80cb483f035a4e4d35cf84b6f",
+    "url": "/static/media/phoneagenda.f1a537c8.bin"
+  }
+]);
